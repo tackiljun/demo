a72d604bade7a2ede48d2215ec9f196e1935781d
tarkIlJun <tack89826@gmail.com>
2023-08-21 23:13:58 +0900

    AWS-test

---
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+@Log4j2
+public class SampleController {
+
+    @GetMapping("/ex1")
+    public String[] ex1(){
+
+        log.info("-----------------------------ex1");
+        return new String[]{"AAA","BBB","CCC"};
+
+    }
+
+}
